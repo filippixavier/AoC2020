c2e5991e6df4545848b36f6bfc9ad8f477e73737
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2020-12-03 10:43:57 +0100

    Third day, second star

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -33,9 +33,8 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let mut tree_count = 0;
 
     for line in forest.iter() {
-        match line.iter().cycle().nth(left_coordinate).unwrap() {
-            Tree => tree_count += 1,
-            Empty => {}
+        if let Tree = line.iter().cycle().nth(left_coordinate).unwrap() {
+            tree_count += 1
         }
         left_coordinate += 3;
     }
@@ -46,5 +45,32 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    use Tile::*;
+    let forest = prepare_input(fs::read_to_string(Path::new("./data/day3.txt"))?);
+    let steps = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let line_len = forest[0].len();
+
+    let mut total_trees: i64 = 1;
+
+    for (step_right, step_down) in &steps {
+        let mut coordinates = (0, 0);
+        let mut trees = 0;
+
+        while coordinates.0 < forest.len() {
+            if let Tree = forest[coordinates.0][coordinates.1] {
+                trees += 1
+            }
+            coordinates.0 += step_down;
+            coordinates.1 = (coordinates.1 + step_right) % line_len;
+        }
+        total_trees *= trees;
+    }
+
+    println!(
+        "Using each pattern, the tree score would be {}",
+        total_trees
+    );
+
     Ok(())
 }
