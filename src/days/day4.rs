0e02cda0fcdf82654129d88bcba5c7287489031c
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2020-12-04 12:09:07 +0100

    Fourth day, second star

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,11 +1,13 @@
+use regex::Regex;
 use std::error::Error;
 use std::fs;
 use std::path::Path;
 
+#[derive(Debug)]
 struct Passport {
-    byr: Option<String>,
-    iyr: Option<String>,
-    eyr: Option<String>,
+    byr: Option<usize>,
+    iyr: Option<usize>,
+    eyr: Option<usize>,
     hgt: Option<String>,
     hcl: Option<String>,
     ecl: Option<String>,
@@ -29,13 +31,13 @@ impl Passport {
             let field = passport_field.split(':').collect::<Vec<_>>();
             match field[0] {
                 "byr" => {
-                    passport.byr = Some(String::from(field[1]));
+                    passport.byr = Some(field[1].parse().unwrap());
                 }
                 "iyr" => {
-                    passport.iyr = Some(String::from(field[1]));
+                    passport.iyr = Some(field[1].parse().unwrap());
                 }
                 "eyr" => {
-                    passport.eyr = Some(String::from(field[1]));
+                    passport.eyr = Some(field[1].parse().unwrap());
                 }
                 "hgt" => {
                     passport.hgt = Some(String::from(field[1]));
@@ -58,6 +60,92 @@ impl Passport {
 
         passport
     }
+
+    fn is_valid(&self) -> bool {
+        if !(self.byr.is_some()
+            && self.iyr.is_some()
+            && self.eyr.is_some()
+            && self.hgt.is_some()
+            && self.hcl.is_some()
+            && self.ecl.is_some()
+            && self.pid.is_some())
+        {
+            return false;
+        }
+        if let Some(byr) = self.byr {
+            if byr < 1920 || byr > 2002 {
+                return false;
+            }
+        }
+
+        if let Some(iyr) = self.iyr {
+            if iyr < 2010 || iyr > 2020 {
+                return false;
+            }
+        }
+
+        if let Some(eyr) = self.eyr {
+            if eyr < 2020 || eyr > 2030 {
+                return false;
+            }
+        }
+
+        if let Some(hgt) = &self.hgt {
+            let reg = Regex::new(r"(\d+)(in|cm)").unwrap();
+            if !reg.is_match(&hgt) {
+                return false;
+            }
+            if let Some(capture) = reg.captures(&hgt) {
+                let value = capture[1].parse::<usize>().unwrap();
+                match &capture[2] {
+                    "in" => {
+                        if value < 59 || value > 76 {
+                            return false;
+                        }
+                    }
+                    "cm" => {
+                        if value < 150 || value > 193 {
+                            return false;
+                        }
+                    }
+                    _ => return false,
+                }
+            }
+        }
+
+        if let Some(hcl) = &self.hcl {
+            if hcl.chars().count() != 7 {
+                return false;
+            }
+
+            let mut hex_value = hcl.chars();
+
+            if hex_value.next().unwrap() != '#' {
+                return false;
+            }
+
+            let value =
+                i32::from_str_radix(hex_value.collect::<String>().as_str(), 16).unwrap_or(-1);
+            let max_value = i32::from_str_radix("ffffff", 16).unwrap();
+
+            if value < 0 || value > max_value {
+                return false;
+            }
+        }
+
+        match self.ecl.as_ref().unwrap().as_str() {
+            "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => {}
+            _ => return false,
+        }
+
+        if let Some(pid) = &self.pid {
+            if pid.chars().count() != 9 || pid.parse::<i32>().unwrap_or(-1) < 0 {
+                return false;
+            }
+        }
+
+        true
+    }
 }
 
 fn prepare_input(input: String) -> Vec<Passport> {
@@ -72,7 +160,7 @@ fn prepare_input(input: String) -> Vec<Passport> {
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let input = prepare_input(fs::read_to_string(Path::new("./data/day4.txt"))?);
-    let valid_passwords = input
+    let valid_passports = input
         .iter()
         .filter(|passport| {
             passport.byr.is_some()
@@ -85,12 +173,18 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         })
         .count();
 
-    println!("Number of valid passwords: {}", valid_passwords);
+    println!("Number of valid passports: {}", valid_passports);
 
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    // let input = fs::read_to_string(Path::new("./data/day4.txt"))?;
+    let input = prepare_input(fs::read_to_string(Path::new("./data/day4.txt"))?);
+    let valid_passports = input.iter().filter(|passport| passport.is_valid());
+
+    println!(
+        "Number of *really* valid passports: {}",
+        valid_passports.count()
+    );
     Ok(())
 }
