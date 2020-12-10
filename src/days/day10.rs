113c0108a712d528188cef8b8a684b80396df56a
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2020-12-10 11:59:17 +0100

    Tenth day, second star

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -50,5 +50,48 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    use std::collections::HashMap;
+
+    let mut adapters = prepare_input(fs::read_to_string(Path::new("./data/day10.txt"))?);
+    adapters.sort_unstable();
+    let mut memoize: HashMap<usize, u64> = HashMap::new();
+    let mut count = 0;
+
+    for adapter in adapters {
+        count = 0;
+
+        if adapter == 1 || adapter == 2 || adapter == 3 {
+            count += 1;
+        }
+
+        if adapter > 1 {
+            count += if let Some(stored) = memoize.get(&(adapter - 1)) {
+                *stored
+            } else {
+                0
+            }
+        }
+
+        if adapter > 2 {
+            count += if let Some(stored) = memoize.get(&(adapter - 2)) {
+                *stored
+            } else {
+                0
+            }
+        }
+
+        if adapter > 3 {
+            count += if let Some(stored) = memoize.get(&(adapter - 3)) {
+                *stored
+            } else {
+                0
+            }
+        }
+
+        memoize.insert(adapter, count);
+    }
+
+    println!("Total number of combinations: {}", count);
+
     Ok(())
 }
