d9c655fc85300aaf68a52170be9d5bc2a3c63581
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2020-12-12 16:20:19 +0100

    Twelth day, second star

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -16,6 +16,7 @@ enum Direction {
 struct Ship {
     facing: isize,
     coordinates: (isize, isize),
+    waypoint_coordinates: (isize, isize),
     movements: Vec<Direction>,
 }
 
@@ -24,6 +25,7 @@ impl Ship {
         Ship {
             facing: 90,
             coordinates: (0, 0),
+            waypoint_coordinates: (1, 10),
             movements: vec![],
         }
     }
@@ -67,11 +69,68 @@ impl Ship {
         }
     }
 
+    fn rotate_waypoint_coordinates(
+        &self,
+        waypoint_coordinates: (isize, isize),
+        orientation: isize,
+    ) -> (isize, isize) {
+        match orientation {
+            90 => (-waypoint_coordinates.1, waypoint_coordinates.0),
+            180 => (-waypoint_coordinates.0, -waypoint_coordinates.1),
+            270 => (waypoint_coordinates.1, -waypoint_coordinates.0),
+            _ => unreachable!(),
+        }
+    }
+
+    fn sail_with_waypoint(&mut self, movement: Direction) {
+        use Direction::*;
+        let mut rotation = None;
+        match movement {
+            North(value) => {
+                self.waypoint_coordinates.0 += value;
+            }
+            East(value) => {
+                self.waypoint_coordinates.1 += value;
+            }
+            South(value) => {
+                self.waypoint_coordinates.0 -= value;
+            }
+            West(value) => {
+                self.waypoint_coordinates.1 -= value;
+            }
+            Forward(value) => {
+                self.coordinates = (
+                    self.coordinates.0 + self.waypoint_coordinates.0 * value,
+                    self.coordinates.1 + self.waypoint_coordinates.1 * value,
+                );
+            }
+            Left(value) => {
+                rotation = Some(360 - value);
+            }
+            Right(value) => {
+                rotation = Some(value);
+            }
+        }
+        if let Some(rotate) = rotation {
+            self.waypoint_coordinates =
+                self.rotate_waypoint_coordinates(self.waypoint_coordinates, rotate);
+        }
+    }
+
     fn navigate(mut self) -> Self {
         let movements = self.movements;
         self.movements = vec![];
         for movement in movements {
             self.sail(movement);
+        }
+        self
+    }
+
+    fn navigate_using_waypoint(mut self) -> Self {
+        let movements = self.movements;
+        self.movements = vec![];
+        for movement in movements {
+            self.sail_with_waypoint(movement);
         }
         self
     }
@@ -113,5 +172,11 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let mut ship = prepare_input(fs::read_to_string(Path::new("./data/day12.txt"))?);
+    ship = ship.navigate_using_waypoint();
+    println!(
+        "Ship navigated {} miles thanks to the waypoint!",
+        ship.coordinates.0.abs() + ship.coordinates.1.abs()
+    );
     Ok(())
 }
