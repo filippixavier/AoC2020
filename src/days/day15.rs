fa3c3992284172b34f04211d84279a2624c1d8cc
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2020-12-15 11:16:20 +0100

    Fifteenth day, second star

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -35,5 +35,24 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    use std::collections::HashMap;
+    let mut input = prepare_input(fs::read_to_string(Path::new("./data/day15.txt"))?).into_iter();
+    let mut positions = HashMap::<usize, usize>::new();
+
+    let mut next_value = 0;
+    let mut current_value = 0;
+
+    for i in 1..=30_000_000 {
+        current_value = input.next().unwrap_or(next_value);
+        if let Some(position) = positions.get_mut(&current_value) {
+            next_value = i - *position;
+            *position = i;
+        } else {
+            positions.insert(current_value, i);
+            next_value = 0;
+        }
+    }
+    println!("{}", current_value);
+
     Ok(())
 }
