b6441a3ecdce79c9b48f925e1838b7442b12a7d3
Xavier FILIPPI <filippi.xavier@yahoo.fr>
2020-12-23 22:03:10 +0100

    Twenty-third day, second star

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -12,54 +12,13 @@ fn prepare_input(input: String) -> Vec<usize> {
         .unwrap()
 }
 
-// pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-//     let mut cups = prepare_input(fs::read_to_string(Path::new("./data/day23.txt"))?);
-
-//     let len = cups.len();
-
-//     for i in 0..10 {
-//         println!("CUPS: {:?}", cups);
-
-//         let index = i % len;
-//         let mut target_id = cups[index];
-//         let took_cup = cups.iter().cycle().skip(index + 1).take(3);
-
-//         let first_part = cups.iter().take(index + 1);
-//         let intermediate = first_part.chain(cups.iter().cycle().skip(index + 4).take(len.checked_sub(index + 4).unwrap_or(0))).cloned().collect::<Vec<_>>();
-//         println!("|{}|", target_id);
-
-//         let mut found_pos = None;
-
-//         while found_pos.is_none() {
-//             if target_id == 0 {
-//                 target_id = 10;
-//             }
-//             target_id -= 1;
-//             found_pos = intermediate.iter().position(|&x| x == target_id);
-//         }
-
-//         if let Some(position) = found_pos {
-//             let first_part = intermediate.iter().take(position + 1);
-//             let second_part = intermediate.iter().skip(position + 1);
-//             cups = first_part.chain(took_cup).chain(second_part).cloned().collect();
-//         }
-//     }
-
-//     let cup_one = cups.iter().position(|&x| x == 1).unwrap();
-
-//     println!("{}", cups.iter().skip(cup_one).fold(1, |acc, &value| acc * value));
-
-//     Ok(())
-// }
-
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let cups = prepare_input(fs::read_to_string(Path::new("./data/day23.txt"))?);
-    let mut cycled_cups: HashMap<usize, usize> = HashMap::new();
-
-    for (index, &cup_no) in cups.iter().enumerate() {
-        let cup_neighbor = cups[(index + 1) % cups.len()] - 1;
-        cycled_cups.insert(cup_no - 1, cup_neighbor);
-    }
+    let mut cycled_cups: HashMap<usize, usize> = cups
+        .iter()
+        .enumerate()
+        .map(|(index, value)| (*value - 1, cups[(index + 1) % cups.len()] - 1))
+        .collect::<HashMap<usize, usize>>();
 
     let mut current_cup = cups[0] - 1;
 
@@ -81,9 +40,7 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         current_cup = next;
     }
 
-    while current_cup != 0 {
-        current_cup = *cycled_cups.get(&current_cup).unwrap();
-    }
+    current_cup = 0;
 
     let mut result = String::new();
 
@@ -98,5 +55,60 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let first_cups_order = prepare_input(fs::read_to_string(Path::new("./data/day23.txt"))?);
+    let mut cups = (1..=1_000_000).collect::<Vec<usize>>();
+
+    for (index, &value) in first_cups_order.iter().enumerate() {
+        cups[index] = value;
+    }
+
+    let mut cycled_cups: HashMap<usize, usize> = cups
+        .iter()
+        .enumerate()
+        .map(|(index, value)| (*value, cups[(index + 1) % cups.len()]))
+        .collect::<HashMap<usize, usize>>();
+    let mut current_cup = cups[0];
+
+    for _ in 0..10_000_000 {
+        let pick1 = *cycled_cups.get(&current_cup).unwrap();
+        let pick2 = *cycled_cups.get(&pick1).unwrap();
+        let pick3 = *cycled_cups.get(&pick2).unwrap();
+        let next = *cycled_cups.get(&pick3).unwrap();
+
+        cycled_cups.insert(current_cup, next);
+
+        let mut destination = if current_cup == 1 {
+            1_000_000
+        } else {
+            current_cup - 1
+        };
+        while destination == pick1
+            || destination == pick2
+            || destination == pick3
+            || destination == current_cup
+        {
+            destination = if destination == 1 {
+                1_000_000
+            } else {
+                destination - 1
+            };
+        }
+
+        cycled_cups.insert(pick3, *cycled_cups.get(&destination).unwrap());
+        cycled_cups.insert(destination, pick1);
+        current_cup = next;
+    }
+    let result;
+
+    let first_cup = *cycled_cups.get(&1).unwrap();
+    let second_cup = *cycled_cups.get(&first_cup).unwrap();
+
+    result = first_cup as u64 * second_cup as u64;
+
+    println!(
+        "Cups are {}, {}, result is: {}",
+        first_cup, second_cup, result
+    );
+
     Ok(())
 }
